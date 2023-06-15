40132fca788cb0814ee5fff8ec2b4b50116ea13d
mohamad-jaallouk <xzxz22@gmail.com>
2023-06-15 10:28:44 +0000

    refactor: change social links

--- src/config.ts
+++ src/config.ts
@@ -23,7 +23,7 @@ export const LOGO_IMAGE = {
 export const SOCIALS: SocialObjects = [
   {
     name: "Github",
-    href: "https://github.com/satnaing/astro-paper",
+    href: "https://github.com/Mohamad-Jaallouk",
     linkTitle: ` ${SITE.title} on Github`,
     active: true,
   },
@@ -31,17 +31,17 @@ export const SOCIALS: SocialObjects = [
     name: "Facebook",
     href: "https://github.com/satnaing/astro-paper",
     linkTitle: `${SITE.title} on Facebook`,
-    active: true,
+    active: false,
   },
   {
     name: "Instagram",
     href: "https://github.com/satnaing/astro-paper",
     linkTitle: `${SITE.title} on Instagram`,
-    active: true,
+    active: false,
   },
   {
     name: "LinkedIn",
-    href: "https://github.com/satnaing/astro-paper",
+    href: "https://www.linkedin.com/in/mohamad-jaallouk",
     linkTitle: `${SITE.title} on LinkedIn`,
     active: true,
   },
